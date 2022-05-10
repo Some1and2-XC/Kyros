b307252811a060e76a428931691e3d0c3d990800
some1and2 <89313812+some1and2and3@users.noreply.github.com>
2022-05-10 17:45:27 -0300

    Name Change
    Changes the code to change the name from Kairos to Kyros

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(
-	name="Kairos",
+	name="Kyros",
 	packages=find_packages(include=["Kairos"]),
 	version="3.0.0",
 	description="A Class Based Python Library for Fractal Generation",
